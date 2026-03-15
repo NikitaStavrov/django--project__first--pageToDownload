643424331ebf73fd73ad04909fbca4cfaf520430
NikitaStavrov <stavrovnk@gmail.com>
2026-03-15 23:44:40 +0100

    Update settings.py

--- test_stuff/firstPageToDownload/settings.py
+++ test_stuff/firstPageToDownload/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '0.0.0.0', '0.0.0.0:8000', 'django-project-first-pagetodownload.onrender.com']
 
